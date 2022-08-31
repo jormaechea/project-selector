2f0d3106e55a53a25929550f33b47c2b7527d027
Manuel Vilche <manuel@fizzmod.com>
2022-08-31 07:06:10 -0300

    improve prompts options

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ const prompts = require('prompts');
 
 const BASE_PATH = '/var/www/';
 
-const readProyects = () => {
-	return fs.readdir(BASE_PATH);
-};
+const readProjects = async () => {
+	const directories = await fs.readdir(BASE_PATH, { withFileTypes: true });
+    return directories.filter(project => project.isDirectory())
+    	.map(project => project.name)
+}
 
-const promptProyects = proyects => {
+const promptProjects = projects => {
 
-	const proyectsChoices = proyects.map(proyect => ({ title: proyect, value: proyect }));
+	const projectsChoices = projects.map(project => ({ title: project, value: project }));
 
 	return prompts([
 		{
 			type: 'autocomplete',
-			name: 'proyect',
-			message: 'Seleccioná el proyecto que quieras',
-			choices: proyectsChoices,
+			name: 'project',
+			message: 'Seleccioná el projecto que quieras',
+			choices: projectsChoices,
 			suggest: (input, choices) => {
 				const matcher = new RegExp(input, 'i');
 				return Promise.resolve(choices.filter(({ title }) => title.match(matcher)));
@@ -28,18 +30,18 @@ const promptProyects = proyects => {
 	])
 };
 
-const selectProyect = proyect => {
-	fs.writeFileSync(path.resolve(__dirname, '.selected-proyect'), proyect);
+const selectProject = project => {
+	fs.writeFileSync(path.resolve(__dirname, '.selected-project'), project);
 };
 
 (async () => {
 
-	selectProyect('');
+	selectProject('');
 
-	const proyects = await readProyects();
+	const projects = await readProjects();
 
-	const { proyect } = await promptProyects(proyects);
+	const { project } = await promptProjects(projects);
 
-	selectProyect(proyect)
+	selectProject(project)
 
 })();
